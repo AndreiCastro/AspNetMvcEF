d9b874e2dd03ad50c16f0203c86a5643ee5cf32f
andreisc <andrei.castro1991@gmail.com>
2023-09-15 16:48:20 -0300

    Incluindo novas validacoes na model

--- ControleContatos/Models/ContatoModel.cs
+++ ControleContatos/Models/ContatoModel.cs
@@ -6,21 +6,25 @@ namespace ControleContatos.Models
     {
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "Digite o nome do contato")]
-        [Display(Name ="TestName")]
+        [Required(ErrorMessage = "Nome é obrigatório.")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Nome deve ter entre {2} a {1} caracteres.")]
         public string Nome { get; set; }
 
-        [Required(ErrorMessage = "Digite o sobrenome do contato")]
+        [Required(ErrorMessage = "Sobrenome é obrigatório.")]
+        [StringLength(150, MinimumLength = 3, ErrorMessage = "Sobrenome deve ter entre {2} a {1} caracteres.")]
         public string Sobrenome { get; set; }
 
-        [Required(ErrorMessage = "Digite o e-mail do contato")]
-        [EmailAddress(ErrorMessage = "O e-mail informado não é válido")]
+        [Required(ErrorMessage = "E-mail é obrigatório.")]
+        [EmailAddress(ErrorMessage = "E-mail informado inválido.")]
+        [Display(Name ="E-mail")]
         public string Email { get; set; }
 
         [Phone(ErrorMessage = "O Telefone informado é inválido")]
+        [StringLength(20, MinimumLength = 7, ErrorMessage = "Telfone deve ter entre {2} a {1} caracteres.")]
         public string Telefone { get; set; }
 
         [Required(ErrorMessage = "Digite o documento do contato")]
+        [StringLength(15, MinimumLength = 8, ErrorMessage = "Documento deve ter entre {2} a {1} caracteres.")]
         public string Documento { get; set; }
     }
 }
